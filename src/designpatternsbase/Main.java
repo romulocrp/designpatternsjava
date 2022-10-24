6dd7e27f3cf14b97562395864ec8a8f8b07dcac7
romulo <romulolespaul@gmail.com>
2022-10-24 10:16:18 -0300

    Interface and coupling concepts practice.

--- src/designpatternsbase/Main.java
+++ src/designpatternsbase/Main.java
@@ -2,7 +2,13 @@ package designpatternsbase;
 
 public class Main {
     public static void main(String[] args) {
+        TaxCalculator calculator = getCalculator();
+        calculator.calculateTax();
         User user = new User("romulo") ;
         user.SayHello();
+    }
+
+    public static TaxCalculator getCalculator(){
+        return new TaxCalculator2019();
     }
 }
---
+++ src/designpatternsbase/TaxCalculator.java
@@ -0,0 +1,5 @@
+package designpatternsbase;
+
+public interface TaxCalculator {
+    float calculateTax();
+}
